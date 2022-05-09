bcf43a6d250b2933c82270e2c59f9a355115c3a2
Jasper <jasper.stephens1@gmail.com>
2022-05-08 20:34:55 -0500

    works hopefiuly

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
     res.send('HelloWorld')
 })
 
 app.listen(port)
+console.log(port)
